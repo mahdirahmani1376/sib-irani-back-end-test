57c9028a62f95d3a9935ff3796b62c4e72eb4094
mahdi <rahmanimahdi16@gmail.com>
2026-07-23 23:27:53 +0330

    (fix) REFACTOR

--- setup.sh
+++ setup.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-
-echo "UID=$(id -u)" >> .env
-echo "GID=$(id -g)" >> .env
-
 cp ./src/.env.example ./src/.env
 cp ./src/.env.testing.example ./src/.env.testing
 
@@ -10,5 +6,5 @@ docker compose up -d --build
 
 docker exec -it sb-php composer install
 docker exec -it sb-php php artisan migrate --force --seed
-docker exec -it sb-php php artisan key:generate --env=testing
-docker exec -it sb-php php artisan test
+docker exec -it sb-php php artisan key:generate --env=.env.testing
+docker exec -it sb-php php artisan test --env=.env.testing
